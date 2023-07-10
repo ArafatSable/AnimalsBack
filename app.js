3ccace23b1af1158cfde68364d847f81d00ae271
ArafatSable <95566091+ArafatSable@users.noreply.github.com>
2023-07-10 23:38:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(require("./router/users"));
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
